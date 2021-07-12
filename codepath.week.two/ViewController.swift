d0ad1090e327f97a41981a9e5ecd9131c53198d1
michael <michael@rowaad.net>
2021-07-12 11:48:00 +0200

    Initial Commit

---
+++ codepath.week.two/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  codepath.week.two
+//
+//  Created by Michael on 12/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
